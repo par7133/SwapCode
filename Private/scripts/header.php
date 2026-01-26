314179b773b59f7ea003ae7dc858e6115c37af07
Dan <my25mb@aol.com>
2026-01-26 11:23:51 +0100

    Update header.php

--- Private/scripts/header.php
+++ Private/scripts/header.php
@@ -83,7 +83,7 @@ Page::clearJSBuffer();
     //$pageKeywords = str_replace(".", PHP_STR, $pageKeywords);
   ?>
   <meta name="keywords" content="<?php echo $pageKeywords;?>"/>
-  <meta name="author" content="5 Mode"/> 
+  <meta name="author" content="NuMode"/> 
   
   <script src="/js/jquery-3.6.0.min.js" type="text/javascript"></script>
     <script src="/js/common.js?v=<?php echo time();?>" type="text/javascript"></script>
@@ -121,7 +121,7 @@ Page::clearJSBuffer();
 </div>
 
   <div class="header2" style="margin-top:18px; margin-left:38px; margin-bottom:18px; color:#000000; display:none;">
-    &nbsp;&nbsp;<a href="http://swapcode.5mode-foss.eu" target="_self" style=" text-decoration: none;"><img src="/res/code.png" style="width:25px;">&nbsp;SwapCode</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/par7133/SwapCode" style=""><span style="">on</span> github</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:code@gaox.io" style=""><span style="">for</span> feedback</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="tel:+39-378-0812391" style="font-size:13px;background-color:#15c60b;border:2px solid #15c60b;color:#000000;height:27px;text-decoration:none;">&nbsp;&nbsp;get support&nbsp;&nbsp;</a>&nbsp;&nbsp;&nbsp;<span class="myxs"><input type="password" id="Password" name="Password" placeholder="new password" style="font-size:11px;">&nbsp;&nbsp;<a href="#" onclick="showEncodedPassword();" style="position:relative; left:0px; top:+2px; font-size:13px; font-weight:900; color:#518dc1;"><?php echo(getResource("Hash Me", $curLocale));?>!</a><span>
+    &nbsp;&nbsp;<a href="http://swapcode.numode-foss.eu" target="_self" style=" text-decoration: none;"><img src="/res/code.png" style="width:25px;">&nbsp;SwapCode</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/par7133/SwapCode" style=""><span style="">on</span> github</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:code@gaox.io" style=""><span style="">for</span> feedback</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="tel:+39-378-0812391" style="font-size:13px;background-color:#15c60b;border:2px solid #15c60b;color:#000000;height:27px;text-decoration:none;">&nbsp;&nbsp;get support&nbsp;&nbsp;</a>&nbsp;&nbsp;&nbsp;<span class="myxs"><input type="password" id="Password" name="Password" placeholder="new password" style="font-size:11px;">&nbsp;&nbsp;<a href="#" onclick="showEncodedPassword();" style="position:relative; left:0px; top:+2px; font-size:13px; font-weight:900; color:#518dc1;"><?php echo(getResource("Hash Me", $curLocale));?>!</a><span>
  </div>     
      
   <div class="body-area">
